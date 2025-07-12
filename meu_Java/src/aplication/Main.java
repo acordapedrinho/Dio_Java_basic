b04075c6b83cf8c6e27f3f06fea112bb4df17f40
Pedro <pedroprocuroemprego@gmail.com>
2025-07-12 11:29:52 -0300

    entrada e saida de valores

---
+++ meu_Java/src/aplication/Main.java
@@ -0,0 +1,52 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main{
+ public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner=new Scanner(System.in);
+
+        Account Account;
+
+        System.out.println("enter com numero");
+        int number= scanner.nextInt();
+        System.out.println("account holder dono:");
+        scanner.nextLine();
+        String holder = scanner.nextLine();
+        System.out.println("is there an initial deposit (y/n)?");
+        char response = scanner.next().charAt(0);
+        if (response=='y'){
+            System.out.println("enter initial deposti value ::");
+            double initalDeposit= scanner.nextDouble();
+
+            account=new Account(number,holder,initalDeposit);
+        }else{
+            account =new Account(number,holder);
+        }
+
+        System.out.println();
+        System.out.println("account data");
+        System.out.println(account);
+
+
+        System.out.println();
+        System.out.print("enter a deposit value");
+        double depositValue =scanner.nextDouble();
+        account.deposit(depositValue);
+        System.out.println("update account data");
+        System.out.println(account);
+
+
+        System.out.println();
+        System.out.print("enter a deposit value");
+        double withdrawValue =scanner.nextDouble();
+        account.withdraw(withdrawValue);
+        System.out.println("update account data");
+        System.out.println(account);
+
+        scanner.close();
+     }
+
+}
